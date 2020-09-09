5c17fe39fb2116c90ce9903b6b2e5c7e2052befc
Anggrayudi H <anggrayudi@users.noreply.github.com>
2020-09-10 01:15:43 +0700

    Add executable permission

--- .github/scripts/copy_env_variables.sh
+++ .github/scripts/copy_env_variables.sh
@@ -3,6 +3,6 @@ echo "$SECRET_KEY_RING_FILE_BASE_64" | base64 --decode >secring.gpg
 echo -e "\nsigning.secretKeyRingFile=secring.gpg" >>gradle.properties
 echo "signing.keyId=$SIGNING_KEY_ID" >>gradle.properties
 echo "signing.password=$SIGNING_PASSWORD" >>gradle.properties
-# Copy secret keys to gradle.properties
+# Copy secret keys into gradle.properties
 echo "SONATYPE_NEXUS_USERNAME=$OSS_SONATYPE_NEXUS_USERNAME" >>gradle.properties
 echo "SONATYPE_NEXUS_PASSWORD=$OSS_SONATYPE_NEXUS_PASSWORD" >>gradle.properties
