47191806f765f9221d8d22a81a7538d9395dd5b6
Anggrayudi H <anggrayudi@users.noreply.github.com>
2020-09-10 01:25:17 +0700

    Move secring to home directory

--- .github/scripts/copy_env_variables.sh
+++ .github/scripts/copy_env_variables.sh
@@ -1,6 +1,6 @@
 # Copy secret key ring for file signature
-echo "$SECRET_KEY_RING_FILE_BASE_64" | base64 --decode >secring.gpg
-echo -e "\nsigning.secretKeyRingFile=secring.gpg" >>gradle.properties
+echo "$SECRET_KEY_RING_FILE_BASE_64" | base64 --decode >"$HOME/"secring.gpg
+echo -e "\nsigning.secretKeyRingFile=$HOME/secring.gpg" >>gradle.properties
 echo "signing.keyId=$SIGNING_KEY_ID" >>gradle.properties
 echo "signing.password=$SIGNING_PASSWORD" >>gradle.properties
 # Copy secret keys into gradle.properties
