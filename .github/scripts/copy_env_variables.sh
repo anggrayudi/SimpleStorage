5b4da2a1594d2bf7dd21d6f7b96ae0de2e988957
Anggrayudi H <anggrayudi@users.noreply.github.com>
2020-12-13 20:18:51 +0700

    mkdir $HOME/.android

--- .github/scripts/copy_env_variables.sh
+++ .github/scripts/copy_env_variables.sh
@@ -4,7 +4,9 @@ if [[ ! ($(grep "STORAGE_VERSION=" gradle.properties) == *"-SNAPSHOT") ]]; then
   rm -f gradle.propertiese
 fi
 
+mkdir "$HOME/.android"
 echo "$DEBUG_KEYSTORE_BASE64" | base64 --decode >"$HOME/.android/debug.keystore"
+
 # Copy secret key ring for file signature
 echo "$SECRET_KEY_RING_FILE_BASE_64" | base64 --decode >"$HOME/secring.gpg"
 echo -e "\nsigning.secretKeyRingFile=$HOME/secring.gpg" >>gradle.properties
