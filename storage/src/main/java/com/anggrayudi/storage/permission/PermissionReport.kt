b7d897140151f4af39ef93e527cf8c4b10119087
w2sv <zangenbergjanek@googlemail.com>
2024-07-08 00:05:10 +0200

    Make PermissionReport data class & PermissionResult value class

--- storage/src/main/java/com/anggrayudi/storage/permission/PermissionReport.kt
+++ storage/src/main/java/com/anggrayudi/storage/permission/PermissionReport.kt
@@ -4,7 +4,7 @@ package com.anggrayudi.storage.permission
  * Created on 12/13/20
  * @author Anggrayudi H
  */
-class PermissionReport(
+data class PermissionReport(
     val permission: String,
     val isGranted: Boolean,
     val deniedPermanently: Boolean
--- storage/src/main/java/com/anggrayudi/storage/permission/PermissionResult.kt
+++ storage/src/main/java/com/anggrayudi/storage/permission/PermissionResult.kt
@@ -4,7 +4,8 @@ package com.anggrayudi.storage.permission
  * Created on 12/13/20
  * @author Anggrayudi H
  */
-class PermissionResult(val permissions: List<PermissionReport>) {
+@JvmInline
+value class PermissionResult(val permissions: List<PermissionReport>) {
 
     val areAllPermissionsGranted: Boolean
         get() = permissions.all { it.isGranted }
